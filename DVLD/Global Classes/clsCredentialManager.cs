226418d3602f8304c6a20d990995ebdbf9f0b0bf
SaeedJ4ber <saeed200m6@gmail.com>
2025-07-27 07:31:57 +0300

    Initial Commit

---
+++ DVLD/Global Classes/clsCredentialManager.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Buisness;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace DVLD.Classes
+{
+   
+
+        internal static class clsCredentialManager
+        {
+            public static clsUser CurrentUser;
+
+            private static string KeyPath = @"SOFTWARE\DVLD_PROJECT\Credentials";
+            private static string ValueName = "UsernameAndPassword";
+            private static string Delimiter = "#//#";
+
+            private static string FullKeyPath = $"HKEY_CURRENT_USER\\{KeyPath}";
+
+            public static bool IsValueDeleted()
+            {
+                try
+                {
+                    using (RegistryKey baseKey = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64))
+                    using (RegistryKey key = baseKey.OpenSubKey(KeyPath, writable: true))
+                    {
+                        if (key == null) return false;
+
+                        key.DeleteValue(ValueName);
+                        return true;
+                    }
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    string sourceName = "DVLD";
+                    if(!EventLog.SourceExists(sourceName))
+                    {
+                        EventLog.CreateEventSource(sourceName, "Application");
+                        MessageBox.Show("UnauthorizedAccessException: Run the program with administrative privileges.");
+
+                    }
+
+                    EventLog.WriteEntry(sourceName, "Unauthorized Access Exception (Error)", EventLogEntryType.Error);
+
+                    MessageBox.Show("UnauthorizedAccessException: Run the program with administrative privileges.");
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                string sourceName = "DVLD";
+                if (!EventLog.SourceExists(sourceName))
+                {
+                    EventLog.CreateEventSource(sourceName, "Application");
+                }
+
+                EventLog.WriteEntry(sourceName, "Exception Occurred (Error)", EventLogEntryType.Error);
+                MessageBox.Show($"An error occurred: {ex.Message}");
+                    return false;
+                }
+            }
+
+            public static bool RememberUserNameAndPassword(string username, string password)
+            {
+                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+                {
+                    return IsValueDeleted();
+                }
+
+                string valueData = FormatCredentials(username, password);
+
+                try
+                {
+                    Registry.SetValue(FullKeyPath, ValueName, valueData, RegistryValueKind.String);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                    return false;
+                }
+            }
+
+            public static bool GetStoredCredentials(ref string username, ref string password)
+            {
+                try
+                {
+                    string value = Registry.GetValue(FullKeyPath, ValueName, null) as string;
+                    if (string.IsNullOrEmpty(value)) return false;
+
+                    ParseCredentials(value, ref username, ref password);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                    return false;
+                    
+                }
+            }
+
+            private static string FormatCredentials(string username, string password)
+            {
+                return $"{username}{Delimiter}{password}";
+            }
+
+            private static void ParseCredentials(string value, ref string username, ref string password)
+            {
+                string[] result = value.Split(new[] { Delimiter }, StringSplitOptions.None);
+
+                username = result[0];
+                password = result[1];
+            }
+        }
+    
+}
---
+++ DVLD/Login/frmLogin.cs
@@ -0,0 +1,96 @@
+﻿using DVLD.Classes;
+using DVLD_Buisness;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            clsUser user= clsUser.FindByUsernameAndPassword(txtUserName.Text.Trim(),txtPassword.Text.Trim());
+
+            if (user != null) 
+            { 
+
+                if (chkRememberMe.Checked )
+                {
+                    //store username and password
+                    clsCredentialManager.RememberUserNameAndPassword(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+
+                } 
+                  else
+                {
+                    //store empty username and password
+                    clsCredentialManager.RememberUserNameAndPassword("", "");
+
+                }
+
+                //incase the user is not active
+                if (!user.IsActive )
+                {
+
+                    txtUserName.Focus();
+                    MessageBox.Show("Your accound is not Active, Contact Admin.", "In Active Account", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    string sourceName = "DVLD";
+                    if (!EventLog.SourceExists(sourceName))
+                    {
+                        EventLog.CreateEventSource(sourceName, "Application");
+                    }
+
+                    EventLog.WriteEntry(sourceName, "Your accound is not Active, Contact Admin.", EventLogEntryType.Error);
+                    return;
+                }
+
+                clsCredentialManager.CurrentUser = user;
+                 this.Hide();
+                 frmMain frm = new frmMain(this);
+                 frm.ShowDialog();
+
+
+            } else
+            {
+                txtUserName.Focus();
+                MessageBox.Show("Invalid Username/Password.", "Wrong Credintials", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }    
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            string UserName = "", Password = "";
+
+            if (clsCredentialManager.GetStoredCredentials(ref UserName, ref Password))
+            {
+                txtUserName.Text = UserName;
+                txtPassword.Text = Password;
+                chkRememberMe.Checked = true;
+            }
+            else
+                chkRememberMe.Checked = false;
+
+        }
+
+       
+    }
+}
